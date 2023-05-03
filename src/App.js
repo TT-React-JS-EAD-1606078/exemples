c2f4296b0ca44b63f791f65f74454e85cd04313b
Luciel Santos <luciel.vitor.bs@gmail.com>
2023-05-02 22:15:47 -0300

    Aula 01

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+export function App() {
+  return (
+    <div>
+      <header>
+        <h1>Aula 01 de React JS</h1>
+
+        <button>Click me</button>
+      </header>
+    </div>
+  );
+}
